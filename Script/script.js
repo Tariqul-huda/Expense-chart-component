bf7475758a1b33de9efe2237ef7bf5e245f495b5
Tariqul Huda <tariqulhuda6@gmail.com>
2022-07-18 16:22:38 +0600

    Second commit

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+
+// expense-chart-mon 
+// expense-chart-tue 
+// expense-chart-wed 
+// expense-chart-thu 
+// expense-chart-fri 
+// expense-chart-sat 
+// expense-chart-sung
+let days;
+let mon = document.getElementById('expense-chart-bar-mon');
+let tue = document.getElementById('expense-chart-bar-tue');
+let wed = document.getElementById('expense-chart-bar-wed');
+let thu = document.getElementById('expense-chart-bar-thu');
+let fri = document.getElementById('expense-chart-bar-fri');
+let sat = document.getElementById('expense-chart-bar-sat');
+let sun = document.getElementById('expense-chart-bar-sun');
